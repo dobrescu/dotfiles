99c884d9188a72cccc97d578b982b7bfe1b328e4
Dan Dobrescu <dobrescu>
2024-11-11 08:43:43 +0100

    style: format install packages script

--- install_sudo.sh
+++ install_sudo.sh
@@ -1,15 +1,19 @@
+# Set up locales
 sudo locale-gen en_US en_US.UTF-8
 sudo dpkg-reconfigure locales
+
+# Install utilities
 sudo apt-get install -y wslu # For WSL2
 sudo apt-get install -y fd-find
-sudo apt install bat
-sudo apt-get install eza
-sudo apt-get install git-delta
+sudo apt-get install -y bat
+sudo apt-get install -y eza
+sudo apt-get install -y git-delta
+sudo apt-get install -y xdg-utils
 
+# Install development tools
 sudo apt-get install -y git jq curl wget
 
-sudo apt-get install xdg-utils
-
+# Install Neovim
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt-get install neovim
+sudo apt-get install -y neovim
