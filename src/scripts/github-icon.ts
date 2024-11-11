0c8a25055f372263c5440911da214069ea173669
Dan Dobrescu <dobrescu>
2024-11-11 09:40:54 +0100

    style: install and run prettier

--- src/scripts/github-icon.ts
+++ src/scripts/github-icon.ts
@@ -1,34 +1,34 @@
 // @ts-nocheck
 
-import { GITUtility } from "jsr:@utility/git";
+import { GITUtility } from 'jsr:@utility/git';
 
 const repo = new GITUtility(Deno.cwd());
-const result = await repo.runCommand("ls-remote", "--get-url");
+const result = await repo.runCommand('ls-remote', '--get-url');
 
-const url = result.startsWith("git@") ? result.replace(/^git@/, "") : result.replace(/^https:\/\@/, "");
+const url = result.startsWith('git@') ? result.replace(/^git@/, '') : result.replace(/^https:\/\@/, '');
 
 const iconMap = {
-    github: "",
-    gitlab: "",
-    bitbucket: "",
-    kernel: "",
-    archlinux: "",
-    gnu: "",
-    git: "",
-    default: "󰊠",
+	github: '',
+	gitlab: '',
+	bitbucket: '',
+	kernel: '',
+	archlinux: '',
+	gnu: '',
+	git: '',
+	default: '󰊠'
 };
 
 type Icons = keyof typeof iconMap;
 
-const [resource, ...x] = url.split(":");
+const [resource, ...x] = url.split(':');
 
-const key = resource.split(".")[0];
+const key = resource.split('.')[0];
 const icon = iconMap[key as Icons] || iconMap.default;
 const repoName = x
-    .join("")
-    .trim()
-    .replace(/\n/g, "")
-    .replace(/\.git$/g, "")
-    .trim();
+	.join('')
+	.trim()
+	.replace(/\n/g, '')
+	.replace(/\.git$/g, '')
+	.trim();
 
 console.log(`${icon} ${repoName}\r`);
